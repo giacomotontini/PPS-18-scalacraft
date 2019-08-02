575b329bcac4928943e4e12d804ff8bc85bcef74
Emiliano Ciavatta <emiliano.ciavatta@studio.unibo.it>
2019-08-02 09:50:17 +0200

    Remove failing code from entrypoint

--- src/main/scala/io/scalacraft/Entrypoint.scala
+++ src/main/scala/io/scalacraft/Entrypoint.scala
@@ -1,27 +1,9 @@
 package io.scalacraft
 
-import java.io.{BufferedInputStream, BufferedOutputStream, ByteArrayInputStream, ByteArrayOutputStream}
-
-import io.scalacraft.core.Marshallers.VarIntMarshaller
-import io.scalacraft.core.serverbound.PlayPackets
-import io.scalacraft.core.serverbound.PlayPackets.{AddPlayer, Direction, PlayerInfo, TestPacket}
-import io.scalacraft.core.{Helpers, PacketManager}
-
 import scala.language.postfixOps
 
 object Entrypoint extends App {
-  val pm = new PacketManager[PlayPackets.type]
-  val originalPacket = Helpers.hex2bytes("57407268196e69a5a240514000")
-  val serializedPacket = new ByteArrayOutputStream()
-  implicit val inStream: BufferedInputStream = new BufferedInputStream(new ByteArrayInputStream(originalPacket))
-  implicit val outStream: BufferedOutputStream = new BufferedOutputStream(serializedPacket)
-  val testPacket = TestPacket(Direction.East)
-  testPacket.productElement(0x7)
-  println(VarIntMarshaller.unmarshal())
-  // pm.marshal(testPacket)
-  //inStream.close()
-  outStream.close()
-  // println(serializedPacket.toByteArray.toList)
-  //assert(originalPacket.toList sameElements serializedPacket.toByteArray)
+
+  // TODO: add entrypoint
 
 }
