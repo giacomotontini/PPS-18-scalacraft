b74df711dacbd37bc57d4f34bc2cb4f46e4d8c8d
Daniele <daniele.rossi18@studio.unibo.it>
2019-08-04 12:17:45 +0200

    Finished server play packets

--- src/main/scala/io/scalacraft/core/serverbound/PlayPackets.scala
+++ src/main/scala/io/scalacraft/core/serverbound/PlayPackets.scala
@@ -350,52 +350,11 @@ object PlayPackets {
   @packet(0x28)
   case class Spectate(targetPlayer: UUID) extends Structure
 
-  /*@packet(0x29)
-  case class PlayerBlockPlacement(location: Position, @enumType[VarInt] face: Face, @enumType[VarInt] ) extends Structure*/
+  @packet(0x29)
+  case class PlayerBlockPlacement(location: Position, @enumType[VarInt] face: Face, @enumType[VarInt] hand: Hand,
+                                  cursorPositionX: Float, cursorPositionY: Float,
+                                  cursorPosiontZ: Float) extends Structure
 
-  sealed trait PlayerInfoAction {
-    val uuid: UUID
-  }
-
-  case class AddPlayerProperty(name: String,
-                               @boxed test: Option[Int],
-                               value: String,
-                               signature: Option[String]) extends Structure
-
-  @switchKey(0)
-  case class AddPlayer(
-                        uuid: UUID,
-                        @maxLength(16) name: String,
-                        @precededBy[VarInt] property: List[AddPlayerProperty],
-                        @boxed gameMode: Int,
-                        @boxed ping: Int,
-                        @maxLength(32767) chat: Option[String]
-                      ) extends PlayerInfoAction with Structure
-
-
-  sealed trait Direction
-
-  object Direction {
-
-    @enumValue(2) case object North extends Direction
-
-    @enumValue(1) case object East extends Direction
-
-  }
-
-  //  @packet(0x30)
-  //  case class PlayerInfo(@switchType[VarInt] @precededBy[VarInt] playerAction: Array[PlayerInfoAction])
-  //    extends Structure
-
-  //  trait Entity
-  //  @packet(0x00)
-  //  case class TestPacket(entityType: Int, foo: Float, @fromContext(0) entity: Entity) extends Structure
-
-  //  @packet(0x01)
-  //  case class TestPacket2(@byte column: Int, @fromContext(0) option: Option[Int]) extends Structure
-
-  @packet(0x24)
-  case class Particle(particleId: Int, particleData: Float, particleCount: Int, @fromContext(0) @switchType[Int] data: ParticleData) extends Structure
-
-
+  @packet(0x2A)
+  case class UseItem(@enumType[VarInt] hand: Hand)
 }
