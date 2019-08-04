052b54d1824e645a21d85d81ab1d3d132da19332
Daniele <daniele.rossi18@studio.unibo.it>
2019-08-04 13:54:56 +0200

    Finish server packets. Need to check some TODO

--- src/main/scala/io/scalacraft/core/serverbound/LoginPackets.scala
+++ src/main/scala/io/scalacraft/core/serverbound/LoginPackets.scala
@@ -1,5 +1,15 @@
 package io.scalacraft.core.serverbound
 
-object LoginPackets {
+import io.scalacraft.core.DataTypes.VarInt
+import io.scalacraft.core.PacketAnnotations.{boxed, maxLength, packet, precededBy}
+import io.scalacraft.core.Structure
 
+object LoginPackets {
+  @packet(0x00)
+  case class LoginStart(@maxLength(16) name: String) extends Structure
+  @packet(0x01)
+  case class EncryptionResponse(@precededBy[VarInt] sharedSecret: List[Byte], @precededBy[VarInt] verifyToken: List[Byte]) extends Structure
+  //TODO
+  @packet(0x02)
+  case class LoginPluginResponse(@boxed messageId: Int, successfull: Boolean/*, data: Option[List[Byte]]*/) extends Structure
 }
