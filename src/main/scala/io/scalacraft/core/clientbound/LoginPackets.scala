51c296e659b4b258b49b67b523ca2e82752b4b23
Daniele <daniele.rossi18@studio.unibo.it>
2019-08-05 17:49:30 +0200

    implement client login packets

--- src/main/scala/io/scalacraft/core/clientbound/LoginPackets.scala
+++ src/main/scala/io/scalacraft/core/clientbound/LoginPackets.scala
@@ -1,5 +1,26 @@
 package io.scalacraft.core.clientbound
 
+import io.scalacraft.core.DataTypes.{Chat, Identifier, VarInt}
+import io.scalacraft.core.PacketAnnotations.{boxed, byte, maxLength, packet, precededBy}
+import io.scalacraft.core.Structure
+
 object LoginPackets {
+
+  @packet(0x00)
+  case class Disconnect(reason: Chat) extends Structure
+
+  @packet(0x01)
+  case class EncryptionRequest(@maxLength(20) serverId: String,
+                               @precededBy[VarInt] @byte publicKey: List[Int],
+                               @precededBy[VarInt] @byte verifyTokenLength: List[Int]) extends Structure
+
+  @packet(0x02)
+  case class LoginSuccess(@maxLength(36) uuid: String, @maxLength(16) username: String) extends Structure
+
+  @packet(0x03)
+  case class SetCompression(@boxed threshold: Int) extends Structure
+
+  @packet(0x04)
+  case class LoginPluginRequest(@boxed messageID: Int, channel: Identifier, @byte data: List[Int]) extends Structure
 
 }
